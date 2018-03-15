3230e449336b250ea089a2d90bbccdf343fa15cf
arodg <anitarodgerson@gmail.com>
2018-03-15 04:06:09 -0500

    Remove apiRoutes to troubleshoot on Heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require("./routes/apiRoutes");
+//const apiRoutes = require("./routes/apiRoutes");
 
 // Serve up static assets
 app.use(express.static("client/build"));
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Use apiRoutes
-app.use("/api", apiRoutes);
+//app.use("/api", apiRoutes);
 
 // Send every request to the React app
 // Define any API routes before this runs
