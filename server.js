f60f1e0f15041896486762f51937feba36c7a902
arodg <anitarodgerson@gmail.com>
2018-03-15 15:31:19 -0500

    Sync sequelize models

--- server.js
+++ server.js
@@ -3,19 +3,20 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3001;
 const app = express();
-//const apiRoutes = require("./routes/apiRoutes");
+
 
 // Serve up static assets
 app.use(express.static("client/build"));
 
-// parse application/x-www-form-urlencoded
+// Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// parse application/json
+// Parse application/json
 app.use(bodyParser.json());
 
-// Use apiRoutes
-//app.use("/api", apiRoutes);
+// Import routes and give the server access to them.
+const routes = require("./controllers/_controllers.js");
+app.use("/", routes);
 
 // Send every request to the React app
 // Define any API routes before this runs
@@ -23,6 +24,11 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> Server now on port ${PORT}!`);
+
+// Sync sequelize models
+var db = require("./models");
+db.sequelize.sync({force: true}).then(function() {
+	app.listen(PORT,function() {
+		console.log("App now listening at localhost:" + PORT);
+	});
 });
