53c17736859704e9b3f8336ea1bb3588955e463d
arodg <anitarodgerson@gmail.com>
2018-03-15 20:15:20 -0500

    Add item model

---
+++ models/item.js
@@ -0,0 +1,40 @@
+var Sequelize = require("sequelize");
+
+module.exports = function(sequelize, DataTypes) {
+  var item = sequelize.define("item", {
+    
+    purchase_request_request_number: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+
+    item_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    serial_number: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+
+    quanitity: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+
+    unit_of_measure: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    }
+  
+  });
+    
+  return item;
+};
+
