b232222ddae5224921877178c7e3d2eb3009d85b
arodg <anitarodgerson@gmail.com>
2018-03-15 20:13:39 -0500

    Add controller

---
+++ controllers/trax_controllers.js
@@ -0,0 +1,41 @@
+
+const express = require("express");
+const router = express.Router();
+
+const db = require("../models");
+
+
+
+router.get("/", function(req, res) {
+  db.item.findAll({}).then(function(data) {
+    var hbsObject = {
+      vendor: data
+    };
+    res.render("index", hbsObject);
+  });
+});
+
+
+router.post("/api/", function(req, res) {
+  db.item.create({
+      item_name: req.body.item_name
+    }).then(function(result) {
+      res.json({id: result.insertID});
+    });
+});
+
+
+router.put("/api/item/:id", function(req, res) {
+  db.item.update({
+    
+  }, {
+    where: {
+      id: req.params.id
+    }
+  }).then(function(result) {
+    res.status(200).end();
+  }); 
+});
+
+
+module.exports = router;
