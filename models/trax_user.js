c946482aacd9ab4390517c1ee6ca4c4f25583e84
arodg <anitarodgerson@gmail.com>
2018-03-15 20:02:55 -0500

    Add trax_user model

---
+++ models/trax_user.js
@@ -0,0 +1,36 @@
+var Sequelize = require("sequelize");
+
+module.exports = function(sequelize, DataTypes) {
+  var vendor = sequelize.define("trax_user", {
+    
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    security_role: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  
+  });
+    
+  return trax_user;
+};
+
+
