2187a6d305e3cac48a4e92562ff23ae202669690
arodg <anitarodgerson@gmail.com>
2018-03-17 04:17:08 -0500

    Add foreign keys to schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,58 +4,55 @@ CREATE DATABASE trax_db;
 USE trax_db;
 
 
-CREATE TABLE trax_user 
+CREATE TABLE employee 
 (
-  id INT AUTO_INCREMENT NOT NULL,
+  employee_id INT AUTO_INCREMENT NOT NULL,
   last_name VARCHAR(25) NOT NULL,
   first_name VARCHAR(25) NOT NULL,
   username VARCHAR(25) NOT NULL,
-  security_role VARCHAR(25) NOT NULL,
   email VARCHAR(50) NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (employee_id)
 );
 
 
 CREATE TABLE vendor
 (
-  id INT AUTO_INCREMENT NOT NULL,
-  vendor_name VARCHAR(100) NOT NULL,
-  description VARCHAR(255),
-  phone VARCHAR(12) NOT NULL,
-  contact_name VARCHAR(50) NOT NULL,
-  email VARCHAR(50) NOT NULL,
+  vendor_id INT AUTO_INCREMENT NOT NULL,
+  company_name VARCHAR(40) NOT NULL,
+  contact_name VARCHAR(40),
+  contact_phone VARCHAR(24),
+  contact_email VARCHAR(50),
   website VARCHAR(50),
-  PRIMARY KEY (id)
+  PRIMARY KEY (vendor_id)
 );
 
 
 CREATE TABLE purchase_request
 (
-  id INT AUTO_INCREMENT NOT NULL,
-  request_number SMALLINT(6) NOT NULL,
+  request_id INT AUTO_INCREMENT NOT NULL,
+  employee_id INT NOT NULL,
+  vendor_id INT NOT NULL,
+  request_date DATE NOT NULL,
+  require_date DATE NOT NULL,
+  completed_date DATE,
   tax_rate DECIMAL(5,2) NOT NULL,
   estimated_shipping DECIMAL(13,4) NOT NULL,
   request_total DECIMAL(13,4) NOT NULL,
-  date_needed DATE NOT NULL,
   justification VARCHAR(255),
-  comments VARCHAR(255),
-  trax_user_id INT NOT NULL,
-  created_date DATE NOT NULL,
-  vendor_id INT NOT NULL,
   status VARCHAR(25),
-  completed_date DATE NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (request_id),
+  FOREIGN KEY (employee_id) REFERENCES employee (employee_id),
+  FOREIGN KEY (vendor_id) REFERENCES vendor (vendor_id)
 );
 
 
 CREATE TABLE item
 (
-  id INT AUTO_INCREMENT NOT NULL,
-  purchase_request_request_number SMALLINT NOT NULL,
+  item_id INT AUTO_INCREMENT NOT NULL,
+  request_id INT NOT NULL,
   item_name VARCHAR(50) NOT NULL,
-  serial_number INT NOT NULL,
-  quantity INT UNSIGNED NOT NULL,
   unit_of_measure VARCHAR(50) NOT NULL,
   price DECIMAL(13,4) NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (item_id),
+  FOREIGN KEY (request_id) REFERENCES purchase_request (request_id)
 );
