6860250513dbbd7ca9547b38d994334918906d24
arodg <anitarodgerson@gmail.com>
2018-03-17 04:36:45 -0500

    Edit model to match changes in schema

--- models/item.js
+++ models/item.js
@@ -3,27 +3,18 @@ var Sequelize = require("sequelize");
 module.exports = function(sequelize, DataTypes) {
   var item = sequelize.define("item", {
     
-    purchase_request_request_number: {
+    item_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+
+    request_id: {
       type: DataTypes.INTEGER,
       allowNull: false
     },
 
     item_name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-
-    serial_number: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-
-    quanitity: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-
-    unit_of_measure: {
       type: DataTypes.STRING,
       allowNull: false
     },
