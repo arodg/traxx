84dfca900f5c59f465e2d58b9362d93827ebec8f
arodg <anitarodgerson@gmail.com>
2018-03-17 13:35:58 -0500

    Edit controller

--- controllers/traxx_controllers.js
+++ controllers/traxx_controllers.js
@@ -6,12 +6,41 @@ const db = require("../models");
 
 
 
-router.get("*", function(req, res) {
-    //res.sendFile(path.join(__dirname + "/../public/index.html"));
-    res.json([{
-    	id: 1,
-    	username: "Pixie"
-    }]);
-});
+// Defining methods for the booksController
+module.exports = {
+  findAll: function(req, res) {
+    db.purchase_request
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.purchase_request
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.purchase_request
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.purchase_request
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.purchase_request
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
 
-module.exports = router;
+
+
