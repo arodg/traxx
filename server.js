3029f7572d416519132ea2e4ce413d97ccb5b66d
arodg <anitarodgerson@gmail.com>
2018-03-18 16:21:16 -0500

    Remove routes to first check deployment to Heroku

--- server.js
+++ server.js
@@ -9,28 +9,34 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Serve up static assets
+Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Traxx' });
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
 // Import routes and give the server access to them.
-var routes = require("./routes");
+//var routes = require("./routes");
 
 // Use apiRoutes
-app.use("/api", routes);
+//app.use("/api", routes);
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+//app.get("*", function(req, res) {
+//  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+//});
 
 
 // Sync sequelize models
-var db = require("./models");
-db.sequelize.sync({force: true}).then(function() {
-	app.listen(PORT,function() {
-		console.log("App now listening at localhost:" + PORT);
-	});
-});
+//var db = require("./models");
+//db.sequelize.sync({force: true}).then(function() {
+//	app.listen(PORT,function() {
+	//	console.log("App now listening at localhost:" + PORT);
+//	});
+//});
