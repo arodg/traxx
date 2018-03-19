a4dcc97bce468336687898fac168bf29c70f3196
arodg <anitarodgerson@gmail.com>
2018-03-19 14:04:17 -0500

    Add request-detail (for purchase requests) model

---
+++ models/request_detail.js
@@ -0,0 +1,48 @@
+var Sequelize = require("sequelize");
+
+module.exports = function(sequelize, DataTypes) {
+  var request_detail = sequelize.define("request_details", {
+    
+    request_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+
+    item_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+
+    unit_price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
+
+    extended_price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
+
+    request_line_number: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+
+  });
+
+  request_detail.associate = function (models) {
+    request_detail.belongsTo(models.purchase_requests, {
+      foreignKey: "request_id"
+    });
+  };
+    
+  return request_detail;
+  
+};
