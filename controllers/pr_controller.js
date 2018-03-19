902ad80ae6031fa2cb1f06ef8bf9bb5ed7882352
arodg <anitarodgerson@gmail.com>
2018-03-19 14:43:48 -0500

    Add contoller files

---
+++ controllers/pr_controller.js
@@ -0,0 +1,41 @@
+const db = require("../models");
+
+
+
+module.exports = {
+  findAll: function(req, res) {
+    db.purchase_request
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.purchase_request
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.purchase_request
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.purchase_request
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.purchase_request
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
+
+
+
