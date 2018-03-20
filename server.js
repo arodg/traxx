1c5dc1513538439484d1d46d6b0af63713b29221
arodg <anitarodgerson@gmail.com>
2018-03-20 01:21:10 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+const routes = require("./routes");
 const app = express();
+const PORT = process.env.PORT || 3001;
+
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,25 +17,21 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Test
-//app.get('/api/hello', (req, res) => {
-//  res.send({ express: 'Hello From Traxx' });
-//});
-
-//app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
-//Import routes and give the server access to them.
-const routes = require("./routes");
-app.use(routes);
+app.get('/', (req, res) => {
+	res.send({ express: 'Hello From Trax' });
+});
 
 // Send every request to the React app
 // Define any API routes before this runs
-//app.get("*", function(req, res) {
-//  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-//});
+app.get("*", function(req, res) {
+	res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
+// Add routes
+app.use(routes);
 
 // Sync sequelize models
-var db = require("./models");
+const db = require("./models");
 db.sequelize.sync({force: false}).then(function() {
 	app.listen(PORT,function() {
 		console.log("App now listening at localhost:" + PORT);
