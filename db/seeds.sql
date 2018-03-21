5f748af71d0c8df9de9f30c53a1b180cb02664e3
arodg <anitarodgerson@gmail.com>
2018-03-21 17:30:41 -0500

    Add required foreign key dummy data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,11 @@
-INSERT INTO employee (last_name, first_name)
+INSERT INTO Employee (last_name, first_name)
 VALUES ("Usrey-Scott", "Jenny"), ("Joselevitz", "Batli"), ("Saenz", "Jason"), ("Rodgerson", "Anita");
 
-INSERT INTO pr (approver_id, request_created, request_required, request_total, tax_rate, estimated_shipping, justification, comments, request_status)
-VALUES ("4", "2018-07-04", "2018-08-04", "30.63", "8.25", "25", "none", "none", "pending");
+INSERT INTO Pr (employee_id, approver_id, request_created, request_required, request_total, tax_rate, estimated_shipping, justification, comments, request_status)
+VALUES ("1", "4", "2018-07-04", "2018-08-04", "30.63", "8.25", "25", "none", "none", "pending");
 
-INSERT INTO item (item_name)
+INSERT INTO Item (item_name)
 VALUES ("computer"), ("chair");
 
-INSERT INTO detail (quantity, unit_price, extended_price, request_line_number)
-VALUES ("3", "20", "60", "4");
+INSERT INTO Detail (request_id, item_id, quantity, unit_price, extended_price, request_line_number)
+VALUES ("2", "1", "3", "20", "60", "4");
