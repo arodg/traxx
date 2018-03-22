6d4f1a12d39724dd98c68c0928ba16b82cd51759
arodg <anitarodgerson@gmail.com>
2018-03-22 11:31:07 -0500

    Add link to login page to direct to home page

--- client/src/components/Form/Input.js
+++ client/src/components/Form/Input.js
@@ -1,6 +1,6 @@
 import React from "react";
 
 export const Input = props =>
-  <div className="form-group">
-    <input className="form-control" {...props} />
+  <div className="form">
+    <input value={props.value} type={props.type} className="form-control" {...props} />
   </div>;
--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import Hello from "../../components/Hello";
-import SecurityRole from "../../components/SecurityRole";
+import { Link } from 'react-router-dom';
 import { Input, FormBtn} from "../../components/Form";
 
 
@@ -26,32 +25,35 @@ class Login extends Component {
         <div id ="welcome">
           WELCOME TO TRAX SYSTEM
           <h2>Manage and track your assets all in one place </h2>
-      </div>
+        </div>
 
         <h1>LOGIN</h1>
         <hr />
-        < Hello />
-        < SecurityRole />
+        
+        
+        <div>
         <form>
           <label htmlFor="Username">Username</label>
-          <Input
-            type="text"
-            name="userName"
-            onChange={this.handleInputChange}
-            value={this.state.userName}
-          />
+            <Input
+              type="text"
+              name="userName"
+              onChange={this.handleInputChange}
+              value={this.state.userName}
+            />
           <label htmlFor="password">Password</label>
-          <Input
-            name="password"
-            onChange={this.handleInputChange}
-            value={this.state.password}
-          />
-          <FormBtn
-                onClick={this.handleFormSubmit}
-              >
-                Submit
-              </FormBtn> 
+            <Input
+              name="password"
+              onChange={this.handleInputChange}
+              value={this.state.password}
+            />
+          <Link to={"/home"}><FormBtn >LOGIN</FormBtn></Link>
+          
         </form>
+      
+        <div className="col-md-4">
+          <img id="clipboard" src="/images/loginimg.jpg" alt="error loading" />
+        </div>
+        </div>
       </div>
     );
   }
